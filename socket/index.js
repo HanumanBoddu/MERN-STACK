8e1ad0c3af986ca26e448b5fe7ee8037049cb0b5
HanumanBoddu <114364054+HanumanBoddu@users.noreply.github.com>
2023-07-27 22:07:04 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,7 @@ import { Server } from 'socket.io';
 
 const io = new Server(9000, {
     cors: {
-        origin: 'https://main--melodious-sawine-fa79e3.netlify.app/',
+        origin: 'https://main--melodious-sawine-fa79e3.netlify.app',
     }, 
 })
 
